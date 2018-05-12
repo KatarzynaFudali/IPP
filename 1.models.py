cc82169f7f8c8a3a8c2c4a89186569fcd79585fd
KatarzynaFudali <269676@uwr.edu.pl>
2018-05-13 01:12:30 +0200

    Update 1.models.py

--- 1.models.py
+++ 1.models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 class Category(models.Model):
-	name = models.CharField(max_length=200, db_index=True)
-	slug = models.SlugField(max_length=200, db_index=True, unique=True)
-	
+	name = models.CharField(max_length=200,
+							db_index=True)
+	slug = models.SlugField(max_length=200,
+							db_index=True,
+							unique=True)
 	class Meta:
 		ordering = ('name',)
 		verbose_name = 'category'
@@ -12,11 +15,17 @@ class Category(models.Model):
 	def _str_(self):
 		return self.name
 
+	def get_absolute_url(self):
+		return reverse('shop:product_list_by_category',
+						args=[self.slug])
+
 class Product(models.Model):
-	category = models.ForeignKey(Category, related_name='products')
+	category = models.ForeignKey(Category,
+								 related_name='products')
 	name = models.CharField(max_length=200, db_index=True)
 	slug = models.SlugField(max_length=200, db_index=True)
-	image = models.ImageField(upload to='products/%Y/%m/%d', blank=True)
+	image = models.ImageField(upload to='products/%Y/%m/%d',
+							  blank=True)
 	description = models.TextField(blank=True)
 	price = models.DecimalField(max_digits=10, decimal_places=2)
 	stock = models.PositiveIntegerField()
@@ -30,3 +39,7 @@ class Product(models.Model):
 
 	def _str_(self):
 		return self.name
+
+	def get_absolute_url(self):
+		return reverse('shop:product_detail',
+						args=[self.id, self.slug])
