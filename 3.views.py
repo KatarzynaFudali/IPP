21b550596fe858ab6be8d07515c4f5beb26d2ff6
KatarzynaFudali <269676@uwr.edu.pl>
2018-05-12 23:37:00 +0200

    Update 3.views.py

--- 3.views.py
+++ 3.views.py
@@ -4,4 +4,8 @@ from .models import Category, Product
 def product_list(request, category_slug=None):
   category = None
   categories = Category.objects.all()
-  products
+  products = Products.objects.filter(available=True)
+  if category_slug:
+      category = get_objets_or_404(Category, slug=category_slug)
+      products = products.filter(category=category)
+  return render(request, 'shop/product/list.html', {'category': category, 'categories': categories, 'products': products})
